076115f230dede6762c89851ceb1527bfc41c7f9
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2023-07-17 23:38:42 +0530

    design card

--- airbnbClone/src/App.jsx
+++ airbnbClone/src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "./data";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Card />
     </div>
   );
 }
--- airbnbClone/src/components/Card.jsx
+++ airbnbClone/src/components/Card.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import { ReactDOM } from "react";
+import katieZaferes from "../assets/katieZaferes.png";
+import Star from "../assets/star.png";
 
 function Navbar() {
-  return <h1>This will be the Card component</h1>;
+  return (
+    <div className="p-14 w-{175px} text-xs">
+      <img src={katieZaferes} alt="" className="" />
+      <div className="flex flex-col">
+        <div className="flex items-center">
+          <img src={Star} alt="" className="h-4" />
+          <span className="text-gray-800">5.0</span>
+          <span className="text-gray-800">(6)</span>
+          <span className="text-gray-800">USA</span>
+        </div>
+        <p className="flex items-start">Life lessons with Katie Zaferes</p>
+        <p className="flex items-start">
+          <span className="font-bold">From $136</span> /person
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
